4cf28d47c143ddd28f2a5ba4b0f429b80c0e41ee
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-11 02:11:58 +0530

    feat(libs): :art: update `popup` component

--- libs/client/popup/src/lib/client-popup.tsx
+++ libs/client/popup/src/lib/client-popup.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/heading-has-content */
 import React from 'react';
 
 import './client-popup.scss';
@@ -6,8 +7,12 @@ export interface IClientPopupProps {} // eslint-disable-line @typescript-eslint/
 
 export const ClientPopup: React.FC = (properties: IClientPopupProps) => {
   return (
-    <div>
-      <h1>Welcome to client-popup!</h1>
+    <div className="popup-container" id="popup-container">
+      <div className="popup">
+        <h2 id="final-message"></h2>
+        <h3 id="final-message-reveal-word"></h3>
+        <button id="play-button">Play Again</button>
+      </div>
     </div>
   );
 };
